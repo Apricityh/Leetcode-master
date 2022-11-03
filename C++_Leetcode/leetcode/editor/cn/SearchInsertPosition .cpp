dcbc9ecdc860f972877470946837168f70f4ba97
Apricityh <1303808701@qq.com>
2022-11-04 00:32:42 +0800

    add the day code

---
+++ C++_Leetcode/leetcode/editor/cn/SearchInsertPosition .cpp
@@ -0,0 +1,73 @@
+
+
+
+
+
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。 
+//
+// 请必须使用时间复杂度为 O(log n) 的算法。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 5
+//输出: 2
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 2
+//输出: 1
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 7
+//输出: 4
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums 为 无重复元素 的 升序 排列数组 
+// -10⁴ <= target <= 10⁴ 
+// 
+//
+// Related Topics 数组 二分查找 👍 1764 👎 0
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n-1,mid ;
+        while (low <= high) {
+            mid = low + (high-low)/2;//这是取二分的mid的方法  高减低除以二再加低
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if(nums[mid]<target) {
+                low = mid+1;
+
+            }
+            else
+                high = mid-1;
+        }
+        return low;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
