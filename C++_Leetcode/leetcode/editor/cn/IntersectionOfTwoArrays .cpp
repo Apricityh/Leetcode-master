7b0fc43b25f0378d29819aae7684bfbd52af1848
Apricityh <1303808701@qq.com>
2023-07-11 11:47:21 +0800

    the code of date 7-11

---
+++ C++_Leetcode/leetcode/editor/cn/IntersectionOfTwoArrays .cpp
@@ -0,0 +1,57 @@
+
+
+
+
+
+//给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums1 = [1,2,2,1], nums2 = [2,2]
+//输出：[2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//输出：[9,4]
+//解释：[4,9] 也是可通过的
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums1.length, nums2.length <= 1000 
+// 0 <= nums1[i], nums2[i] <= 1000 
+// 
+//
+// Related Topics 数组 哈希表 双指针 二分查找 排序 👍 788 👎 0
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    unordered_set<int> result;
+    unordered_set<int> num(nums1.begin(),nums2.end());
+    for(int i:nums2){
+        if (num.find(i)!= num.end()){
+            result.insert(i);
+        }
+    }
+    return vector<int>(num.begin(),num.end()) ;
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
