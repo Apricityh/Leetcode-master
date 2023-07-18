7fe36351a2d0f621a49fb959f3846897e8b67ea4
Apricityh <1303808701@qq.com>
2023-07-19 01:24:12 +0800

    the code of date 7-19

---
+++ C++_Leetcode/leetcode/editor/cn/FourSum .cpp
@@ -0,0 +1,96 @@
+
+
+
+//给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[
+//b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）： 
+//
+// 
+// 0 <= a, b, c, d < n 
+// a、b、c 和 d 互不相同 
+// nums[a] + nums[b] + nums[c] + nums[d] == target 
+// 
+//
+// 你可以按 任意顺序 返回答案 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,0,-1,0,-2,2], target = 0
+//输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [2,2,2,2,2], target = 8
+//输出：[[2,2,2,2]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 200 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 
+//
+// Related Topics 数组 双指针 排序 👍 1677 👎 0
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int> &nums, int target) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for (int k = 0; k < nums.size(); ++k) {
+            if (nums[k] > target && nums[k] > 0) {
+                break;
+            }
+            //对nums[k]去重
+            if (k > 0&&nums[k] == nums[k - 1]  ) {
+                continue;//结束这一次的循环
+            }
+            //二级剪枝
+            for (int i = k + 1; i < nums.size(); ++i) {
+                //剪枝，把nums[k]+nums[i]看作三数之和的第一个数字
+                if (nums[k] + nums[i] > target && nums[k] > 0) {
+                    break;
+                }
+                //对nums[i]去重
+                if (i > k + 1&&nums[i] == nums[i - 1]  ) {
+                    continue;
+                }
+                int left = i + 1;
+                int right = nums.size() - 1;
+                while (left < right) {
+                    if ((long)nums[k] + nums[i] + nums[left] + nums[right] < target) {left++;}//数字太长了会溢出
+                    else if ((long)nums[k] + nums[i] + nums[left] + nums[right] > target) {right--;}
+                    else {
+                        result.push_back(vector<int>{nums[k],nums[i],nums[left],nums[right]});
+                        while (left<right&&nums[left]==nums[left+1]){
+                            left++;
+                        }
+                        while(left<right&&nums[right]==nums[right-1]){
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
