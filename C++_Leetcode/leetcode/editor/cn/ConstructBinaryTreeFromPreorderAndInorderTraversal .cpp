59fa514df5ebb909a98c551acbec35a15262bbda
Apricityh <1303808701@qq.com>
2023-09-07 11:01:47 +0800

    “âthe code of date 9-7 add

--- C++_Leetcode/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal .cpp
+++ C++_Leetcode/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal .cpp
@@ -44,21 +44,52 @@
 using namespace std;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+//Definition for a binary tree node.
+//struct TreeNode {
+//      int val;
+//      TreeNode *left;
+//      TreeNode *right;
+//      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  };
+
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    TreeNode* traversal(vector<int> &preorder,vector<int>& inorder){
+        if (preorder.size()==0){
+            return nullptr;
+        }
+        //找到根节点
+        int nodeValue = preorder[0];
+        TreeNode *root = new TreeNode(nodeValue);
+        if (preorder.size()==1){
+            return root;
+        }
+        //找到中序的根节点所在位置
+        int nodeIndex;
+        for (nodeIndex = 0; nodeIndex < inorder.size(); ++nodeIndex) {
+            if (inorder[nodeIndex]==nodeValue){
+                break;
+            }
+        }
+        //更新中序
+        vector<int> leftInorder(inorder.begin(),inorder.begin()+nodeIndex);
+        vector<int> rightInorder(inorder.begin()+nodeIndex+1,inorder.end());
+        //更新先序
+        vector<int> leftPreorder(preorder.begin()+1,preorder.begin()+1+nodeIndex);
+        vector<int> rightPreoder(preorder.begin()+1+nodeIndex,preorder.end());
 
+        root->left = traversal(leftPreorder,leftInorder);
+        root->right = traversal(rightPreoder,rightInorder);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.size()==0|| inorder.size()==0){
+            return nullptr;
+        }
+        return traversal(preorder,inorder);
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
