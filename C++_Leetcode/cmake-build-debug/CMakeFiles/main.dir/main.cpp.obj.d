f8a4319b36eedb21369ae4403e20ba014af7aaf4
Apricityh <1303808701@qq.com>
2024-03-30 15:19:53 +0800

    the code of target sum

--- C++_Leetcode/cmake-build-debug/CMakeFiles/main.dir/main.cpp.obj.d
+++ C++_Leetcode/cmake-build-debug/CMakeFiles/main.dir/main.cpp.obj.d
@@ -1,49 +1,14 @@
 CMakeFiles/main.dir/main.cpp.obj: \
  G:\Deeping_Learing\Leetcode-master\C++_Leetcode\main.cpp \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\vector \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_algobase.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\iostream \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\i686-w64-mingw32\bits\c++config.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\i686-w64-mingw32\bits\os_defines.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\i686-w64-mingw32\bits\cpu_defines.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\functexcept.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\exception_defines.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\cpp_type_traits.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ext\type_traits.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ext\numeric_traits.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_pair.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\move.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\concept_check.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\type_traits \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_iterator_base_types.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_iterator_base_funcs.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\debug\debug.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_iterator.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\allocator.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\i686-w64-mingw32\bits\c++allocator.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ext\new_allocator.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\new \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\exception \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\atomic_lockfree_defines.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\exception_ptr.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\nested_exception.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\memoryfwd.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_construct.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ext\alloc_traits.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\alloc_traits.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\ptr_traits.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_uninitialized.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_vector.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\initializer_list \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_bvector.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\functional_hash.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\hash_bytes.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\range_access.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\vector.tcc \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\iostream \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ostream \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ios \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\iosfwd \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stringfwd.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\memoryfwd.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\postypes.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\cwchar \
  f:\mingw\mingw32\i686-w64-mingw32\include\wchar.h \
@@ -60,7 +25,25 @@ CMakeFiles/main.dir/main.cpp.obj: \
  f:\mingw\mingw32\i686-w64-mingw32\include\swprintf.inl \
  f:\mingw\mingw32\i686-w64-mingw32\include\sec_api\wchar_s.h \
  f:\mingw\mingw32\i686-w64-mingw32\include\_mingw_print_pop.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\exception \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\atomic_lockfree_defines.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\exception_ptr.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\exception_defines.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\nested_exception.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\char_traits.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_algobase.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\functexcept.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\cpp_type_traits.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ext\type_traits.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ext\numeric_traits.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_pair.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\move.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\concept_check.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\type_traits \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_iterator_base_types.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_iterator_base_funcs.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\debug\debug.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_iterator.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\cstdint \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\stdint.h \
  f:\mingw\mingw32\i686-w64-mingw32\include\stdint.h \
@@ -82,11 +65,17 @@ CMakeFiles/main.dir/main.cpp.obj: \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\i686-w64-mingw32\bits\atomic_word.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\locale_classes.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\string \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\allocator.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\i686-w64-mingw32\bits\c++allocator.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ext\new_allocator.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\new \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\ostream_insert.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\cxxabi_forced.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_function.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\backward\binders.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\range_access.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\basic_string.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\initializer_list \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\ext\string_conversions.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\cstdlib \
  f:\mingw\mingw32\i686-w64-mingw32\include\stdlib.h \
@@ -97,6 +86,8 @@ CMakeFiles/main.dir/main.cpp.obj: \
  f:\mingw\mingw32\i686-w64-mingw32\include\malloc.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\cstdio \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\cerrno \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\functional_hash.h \
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\hash_bytes.h \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\basic_string.tcc \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\locale_classes.tcc \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\streambuf \
@@ -112,35 +103,4 @@ CMakeFiles/main.dir/main.cpp.obj: \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\basic_ios.tcc \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\ostream.tcc \
  f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\istream \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\istream.tcc \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\queue \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\deque \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_deque.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\deque.tcc \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_heap.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_queue.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\algorithm \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\utility \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_relops.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_algo.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\algorithmfwd.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_tempbuf.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\random \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\cmath \
- f:\mingw\mingw32\i686-w64-mingw32\include\math.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\limits \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\random.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\i686-w64-mingw32\bits\opt_random.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\x86intrin.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\ia32intrin.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\immintrin.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\adxintrin.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\random.tcc \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\numeric \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\stl_numeric.h \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\functional \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\typeinfo \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\tuple \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\array \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\stdexcept \
- f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\uses_allocator.h
+ f:\mingw\mingw32\lib\gcc\i686-w64-mingw32\4.8.2\include\c++\bits\istream.tcc
