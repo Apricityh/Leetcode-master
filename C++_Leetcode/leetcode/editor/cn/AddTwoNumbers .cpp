6e1e45263d30724befd06e5eb7190c608b90fada
Apricityh <1303808701@qq.com>
2024-04-20 00:24:31 +0800

    the code of 4-20

--- C++_Leetcode/leetcode/editor/cn/AddTwoNumbers .cpp
+++ C++_Leetcode/leetcode/editor/cn/AddTwoNumbers .cpp
@@ -44,20 +44,21 @@
 
 #include <iostream>
 #include "vector"
+
 using namespace std;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    ListNode() : val(0), next(nullptr) {}
-
-    ListNode(int x) : val(x), next(nullptr) {}
-
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+//struct ListNode {
+//    int val;
+//    ListNode *next;
+//
+//    ListNode() : val(0), next(nullptr) {}
+//
+//    ListNode(int x) : val(x), next(nullptr) {}
+//
+//    ListNode(int x, ListNode *next) : val(x), next(next) {}
+//};
 
 
 
@@ -65,47 +66,35 @@ struct ListNode {
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        {
-            ListNode *head =  nullptr;
-            ListNode *tail = nullptr;
-            int carry = 0;
-            while (l1 || l2) {
-                int n1 = l1?l1->val:0;
-                int n2 = l2?l2->val:0;
-                int sum = n1 + n2 + carry;
-                if(!head ) {
-                    head = tail->next = new ListNode(sum%10);
 
-                }
+        ListNode *head = nullptr, *tail = nullptr;
+        int carry = 0;
+        while (l1 || l2) {
+            int n1 = l1 ? l1->val : 0;
+            int n2 = l2 ? l2->val : 0;
+            int sum = n1 + n2 + carry;
+            if (!head) { head = tail = new ListNode(sum % 10); }
             else {
-                    tail->next = new ListNode(sum%10);
-                    tail= tail->next;
-
+                tail->next = new ListNode(sum % 10);
+                tail = tail->next;
             }
-            carry = sum/10;
-            if(l1){
-            l1 = l1->next;
-            }
-            if(l2){
-            l2 = l2->next;
-            }
-
-            }
-            if(carry > 0){
-                tail->next = new ListNode(carry);}
-            return head;
+            carry = sum / 10;
+            if (l1) { l1 = l1->next; }
+            if (l2) { l2 = l2->next; }
         }
+        if (carry > 0) { tail->next = new ListNode(carry); }
+        return head;
+
     }
 };
 
 //leetcode submit region end(Prohibit modification and deletion)
 
 
-int main()
-{
+int main() {
     Solution s;
     vector<int> data{7, 1, 5, 3, 6, 4};
     //vector<int> ans = s.twoSum(data,11);
     //cout << ans[0]<<ans[1]<<endl;
-    cout<<"Hello LeetCode"<<endl;
+    cout << "Hello LeetCode" << endl;
 }
---
+++ C++_Leetcode/leetcode/editor/cn/DiameterOfBinaryTree .cpp
@@ -0,0 +1,64 @@
+
+
+
+
+
+//给你一棵二叉树的根节点，返回该树的 直径 。 
+//
+// 二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。 
+//
+// 两节点之间路径的 长度 由它们之间边数表示。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,2,3,4,5]
+//输出：3
+//解释：3 ，取路径 [4,2,1,3] 或 [5,2,1,3] 的长度。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,2]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [1, 10⁴] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 1521 👎 0
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
