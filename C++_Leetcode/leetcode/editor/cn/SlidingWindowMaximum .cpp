bec15b0c9e23897eef0274ccb4cf21d452c7e246
Apricityh <1303808701@qq.com>
2024-04-24 00:32:00 +0800

    code reveiw

--- C++_Leetcode/leetcode/editor/cn/SlidingWindowMaximum .cpp
+++ C++_Leetcode/leetcode/editor/cn/SlidingWindowMaximum .cpp
@@ -70,6 +70,24 @@ private:
             return que.front();//返回队头元素
         }
     };
+//class MyQueue{
+//public:
+//    deque<int> que;
+//    void pop(int val){
+//        while(!que.empty()&&que.front()==val){
+//            que.pop_front();
+//        }
+//    }
+//    void push(int val){
+//        while(!que.empty()&&val>que.back()){
+//            que.pop_back();
+//        }
+//        que.push_back(val);
+//    }
+//    int front(){
+//        return que.front();
+//    }
+//};
 public:
     vector<int> maxSlidingWindow(vector<int> &nums, int k) {
             MyQueue que;
@@ -79,11 +97,23 @@ public:
         }
         result.push_back(que.front());
         for (int i = k; i <nums.size() ; ++i) {
-            que.pop(nums[i-k]);//先看能不能满足弹出最开始的那个元素  万一是 3 2 1 开头这样
+           que.pop(nums[i-k]);//先看能不能满足弹出最开始的那个元素  万一是 3 2 1 开头这样
             que.push(nums[i]);  // 把紧接着的一位传入进去。
             result.push_back(que.front());
         }
         return result;
+//        MyQueue que;
+//        vector<int> res;
+//        for (int i = 0; i < k; ++i) {
+//            que.push(nums[i]);
+//        }
+//        res.push_back(que.front());
+//        for (int i = k; i <nums.size() ; ++i) {
+//            que.pop(nums[i-k]);
+//            que.push(nums[i]);
+//            res.push_back(que.front());
+//        }
+//        return res;
      }
 };
 //leetcode submit region end(Prohibit modification and deletion)
